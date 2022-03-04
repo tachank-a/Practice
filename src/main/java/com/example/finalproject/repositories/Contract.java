0753524fed40e48d9a8d60dc9ecdc82baf8f2046
tachank-a <gildo2015@yandex.ru>
2022-03-04 16:20:18 +0700

    1

---
+++ src/main/java/com/example/finalproject/repositories/Contract.java
@@ -0,0 +1,6 @@
+package com.example.finalproject.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface Contract extends PagingAndSortingRepository<Contract,Long> {
+}
